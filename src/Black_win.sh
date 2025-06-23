b8e9d24d661a2550c0993451a6052b8e4734f370
iaceene <yassineajagrou986@gmail.com>
2025-06-23 02:27:05 +0100

    Update : fixing script to check secret file

--- src/Black_win.sh
+++ src/Black_win.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+if [ ! -e ".secret" ]; then
+    echo "No secret key found, Please reinstall"
+    exit 1
+fi
+
+
+
+
 pipe=$(find /dev/shm/ -maxdepth 1 -name '*.pipe' -type p | head -n 1)
 
 if [[ -z "$pipe" ]]; then
     echo "No pipe found matching /dev/shm/*.pipe"
-    exit 1
 fi
 
 echo "Starting cat /dev/random > $pipe in background"
